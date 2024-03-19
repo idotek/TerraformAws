2d2f2b6f77e603b4a6ab0f50a4c548f13ad58225
Matheo <matheo.morelle@icloud.com>
2024-03-19 14:41:39 +0100

    feat: instance ec2 & NSG

--- main.tf
+++ main.tf
@@ -88,7 +88,96 @@ resource "aws_route_table" "TF-Route" {
   }
 }
 
-resource "aws_main_route_table_association" "a" {
+resource "aws_main_route_table_association" "RouteAndVPC" {
   vpc_id         = aws_vpc.TF-VPC.id
   route_table_id = aws_route_table.TF-Route.id
 }
+
+resource "aws_security_group" "WebNSG" {
+    name = "Web NSG"
+    description = "Web NSG"
+    vpc_id = aws_vpc.TF-VPC.id
+
+    ingress {
+        description = "SSH"
+        from_port        = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["10.10.0.0/24"]
+    }
+
+    ingress {
+        description = "Web"
+        from_port        = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+}
+
+resource "aws_instance" "AC-Admin" {
+  instance_type = "t2.micro"
+  ami = "ami-080e1f13689e07408"
+  subnet_id = aws_subnet.AZ-Admin.id
+  vpc_security_group_ids = [aws_security_group.WebNSG.id]
+  tags = {
+    Name = "AC-Admin"
+  }
+}
+
+resource "aws_instance" "AC-A" {
+  instance_type = "t2.micro"
+  ami = "ami-080e1f13689e07408"
+  subnet_id = aws_subnet.AZ-A.id
+  vpc_security_group_ids = [aws_security_group.WebNSG.id]
+  tags = {
+    Name = "AC-A"
+  }
+}
+
+resource "aws_instance" "AC-B" {
+  instance_type = "t2.micro"
+  ami = "ami-080e1f13689e07408"
+  subnet_id = aws_subnet.AZ-B.id
+  vpc_security_group_ids = [aws_security_group.WebNSG.id]
+  tags = {
+    Name = "AC-B"
+  }
+}
+
+resource "aws_instance" "AC-C" {
+  instance_type = "t2.micro"
+  ami = "ami-080e1f13689e07408"
+  subnet_id = aws_subnet.AZ-C.id
+  vpc_security_group_ids = [aws_security_group.WebNSG.id]
+  tags = {
+    Name = "AC-C"
+  }
+}
+
+
+/* resource "aws_lb" "TF-LB" {
+    name = "TF-LB"
+    internal = false
+    load_balancer_type = "application"
+    
+    subnet_mapping {
+      subnet_id = aws_subnet.AZ-A
+    }
+    subnet_mapping {
+      subnet_id = aws_subnet.AZ-B
+    }
+    subnet_mapping {
+      subnet_id = aws_subnet.AZ-C
+    }
+} */
+
